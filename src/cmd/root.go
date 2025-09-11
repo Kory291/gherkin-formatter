7b13eb3c2ec39dff95e4ce97178c1320dafd27df
Kory291 <kory291@proton.me>
2025-09-12 01:58:45 +0200

    basic setup for cmd

---
+++ src/cmd/root.go
@@ -0,0 +1,17 @@
+package cmd
+
+var rootCmd = &cobra.Command{
+	Use: "",
+	Short: "",
+	Long: ""
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
