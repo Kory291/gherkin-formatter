29b25d59bcda42360ea125778141191d2289aaa1
Kory291 <kory291@proton.me>
2025-09-28 19:49:47 +0200

    add basic function to get current directory and see all files

---
+++ cmd/scanFiles.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Kory291/gherkin-formatter/pkg/scanFiles"
+	"github.com/spf13/cobra"
+)
+
+// scanFilesCmd represents the scanFiles command
+var scanFilesCmd = &cobra.Command{
+	Use:   "scanFiles",
+	Short: "Returns all found Feature files",
+	Long: `This command can be used to retrieve a list of all feature files that are discovered by gherkin-formatter`,
+	Run: func(cmd *cobra.Command, args []string) {
+		pwd, err := scanFiles.WhereAmI()
+		if err != nil {
+			os.Exit(1);
+		}
+		fileNames, err := scanFiles.FindFeatureFiles(pwd);
+		if err != nil {
+			os.Exit(1);
+		}
+		fmt.Println(fileNames);
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scanFilesCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// scanFilesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// scanFilesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
